20abb8c3fdd32e0f402835ee587c4b0f05f58bdd
YuFireWhisper <yu.whisper.personal@gmail.com>
2024-10-04 12:08:10 +0800

    0.0.3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,11 @@ function Home() {
     // 以下是頁面的內容，後期應該要轉為使用資料庫
     const eventName = 'Counterspell Taiwan';
     const eventDescriptionText = "全台第一場由青少年為青少年舉辦的黑客松";
-    
+    const hackathonInfoTitle = "甚麼是黑客松?";
+    const hackathonInfoText = "黑客松是一種由程式設計師、設計師、產品經理等多種角色組成的團隊，共同在限定時間內合作完成特定項目的競賽活動。";
+    const teamIntroductionTitle = "團隊介紹";
+    const teamIntroductionText = "我們是一群來自全台各地的青少年，對程式設計、設計、產品開發等領域有著濃厚的興趣。";
+
     useEffect(() => {
         // 查找導航欄元素
         const navbar = document.querySelector('nav');
@@ -20,9 +24,10 @@ function Home() {
 
         // 導航條高度可能會改變，監控 window 的 resize 事件
         const handleResize = () => {
-        if (navbar) {
-            setNavbarHeight(navbar.offsetHeight);
-        }
+            const navbar = document.querySelector('nav');
+            if (navbar) {
+                setNavbarHeight(navbar.offsetHeight);
+            }
         };
 
         // 增加事件監聽器
@@ -36,41 +41,90 @@ function Home() {
 
     return (
         <HomePageContent navbarHeight={navbarHeight}>
-            <EventName>{ eventName }</EventName>
-            <EventDescription>{ eventDescriptionText }</EventDescription>
+            <FirstPage>
+                <EventName>{eventName}</EventName>
+                <EventDescription>{eventDescriptionText}</EventDescription>
+            </FirstPage>
+            <ContentSection>
+                <Section index={0}>
+                    <SectionTitle>{hackathonInfoTitle}</SectionTitle>
+                    <Divider />
+                    <SectionText>{hackathonInfoText}</SectionText>
+                </Section>
+                <Section index={1}>
+                    <SectionTitle>{teamIntroductionTitle}</SectionTitle>
+                    <Divider />
+                    <SectionText>{teamIntroductionText}</SectionText>
+                </Section>
+            </ContentSection>
         </HomePageContent>
     )
 }
 
-
 // 以下是樣式設定
 const HomePageContent = styled.div`
     display: grid;
-    grid-template-columns: 200px 700px 1fr;
-    grid-template-rows: 100px 110px 90px 1fr;
+    grid-template-rows: calc(100vh - ${props => props.navbarHeight}px) auto;
     grid-template-areas:
-        ". . ."
-        ". eventName ."
-        ". eventDescription .";
-    gap: px;
-    height: calc(100vh - ${props => props.navbarHeight}px);
+        "firstPage"
+        "contentSection";
     padding-top: ${props => props.navbarHeight}px;
+    overflow: hidden;
+    scroll-behavior: smooth;
+    scroll-snap-type: y mandatory;
+`;
+
+const FirstPage = styled.div`
+    grid-area: firstPage;
+    display: grid;
+    grid-template-columns: 50px 1fr;
+    grid-template-rows: 100px auto;
+    grid-template-areas:
+        ". eventName"
+        ". eventDescription";
+    align-items: start;
+    padding: 20px;
+    scroll-snap-align: start;
+    height: calc(100vh - ${props => props.navbarHeight}px);
+    min-height: 100vh;
+`;
+
+const ContentSection = styled.div`
+    grid-area: contentSection;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    padding: 20px;
+    scroll-snap-align: start;
+    min-height: calc(100vh - ${props => props.navbarHeight}px);
+`;
+
+const Section = styled.div`
+    display: grid;
+    grid-template-columns: 1fr auto 2fr;
+    align-items: center;
+    background-color: ${props => props.index % 2 === 0 ? '#ffffff' : '#f0f0f0'};
+    padding: 60px;
+    border-radius: 20px;
+    gap: 20px;
+    margin-bottom: 0;
+    margin-top: 0;
 `;
 
 const EventName = styled.h1`
-    grid-area: eventName;
     @font-face {
         font-family: 'Audiowide-Regular';
         src: url('/fonts/Audiowide-Regular.ttf?v=1') format('truetype');
         font-weight: normal;
         font-style: normal;
     }
+    grid-area: eventName;
     font-family: 'Audiowide-Regular';
     font-size: 60px;
     color: black;
     white-space: pre-line;
-    display: grid;
-    align-items: start;
+    justify-self: flex-start;
+    width: 75%;
 `;
 
 const EventDescription = styled.p`
@@ -80,14 +134,38 @@ const EventDescription = styled.p`
         font-weight: normal;
         font-style: normal;
     }
+    grid-area: eventDescription;
     font-family: 'Noto Sans TC';
     font-size: 25px;
-    grid-area: eventDescription;
-    display: grid;
-    align-items: start;
+    align-self: start;
     border-bottom: 2px solid black;
     padding-bottom: 5px;
-    width: 75%;
+    width: 40%;
+`;
+
+const SectionTitle = styled.h2`
+    font-family: 'Noto Sans TC';
+    font-size: 40px;
+    font-weight: 800;
+    align-self: center;
+    justify-self: center;
+    text-align: center;
+`;
+
+const SectionText = styled.p`
+    font-family: 'Noto Sans TC';
+    font-size: 16px;
+    padding-left: 40px;
+    align-self: start;
+    padding-top: 10px;
+    width: 100%;
+`;
+
+const Divider = styled.div`
+    width: 4px;
+    height: 100%;
+    background-color: #8BC34A;
+    border-radius: 5px;
 `;
 
 export default Home;
